1570c142b398be0795910333751936be7b1f7e87
Adrian Xu <741996133@qq.com>
2019-03-04 23:55:56 +0800

    commit code

--- src/collection/SortCollection.java
+++ src/collection/SortCollection.java
@@ -36,11 +36,11 @@ public class SortCollection {
         int temp = numbers[low];
 
         while (low < high) {
-            while (low < high && numbers[high] > temp) {
+            while (low < high && numbers[high] >= temp) {
                 high--;
             }
             numbers[low] = numbers[high];
-            while (low < high && numbers[low] < temp) {
+            while (low < high && numbers[low] <= temp) {
                 low++;
             }
             numbers[high] = numbers[low];
@@ -55,5 +55,83 @@ public class SortCollection {
             quickSort(numbers, low, middle - 1);
             quickSort(numbers, middle + 1, high);
         }
+    }
+
+    public static void quick(int[] numbers) {
+        if (numbers.length > 0) {
+            quickSort(numbers, 0, numbers.length - 1);
+        }
+    }
+
+    /**
+     * Select Sort
+     * Find the smallest element in the unsorted array, put it in the first one of the sorted array.
+     * Find the smallest element in the rest of the array, put it in the end of the sorted array.
+     * Doing this until all the element sorted
+     *
+     * @param numbers
+     */
+    public static void selectSort(int[] numbers) {
+        int temp = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int k = i;
+            for (int j = numbers.length - 1; j > i; j--) {
+                if (numbers[j] < numbers[k]) {
+                    k = j;
+                }
+            }
+            if (i != k) {
+                temp = numbers[i];
+                numbers[i] = numbers[k];
+                numbers[k] = temp;
+            }
+        }
+    }
+
+    /**
+     * Insert Sort
+     * From the first element, consider it's the sorted element
+     * get the next element, scan the sorted element from back to front
+     * if the sorted element bigger than the new element, put it into the next place
+     * repeat this step until find the sorted element smaller than or equal to the new element
+     * insert the new element to this place
+     * repeat
+     *
+     * @param numbers
+     */
+    public static void insertSort(int[] numbers) {
+        int temp = 0;
+        int j = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            temp = numbers[i];
+            for (j = i; j > 0 && temp < numbers[i - 1]; j--) {
+                numbers[j] = numbers[j - 1];
+            }
+            numbers[j] = temp;
+        }
+    }
+
+    public static void printArr(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ",");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 15, 0, 6, 7, 2, 1, -5, 55};
+        System.out.print("排序前：");
+        printArr(numbers);
+
+        bubbleSort(numbers);
+        System.out.print("冒泡排序后：");
+        printArr(numbers);
+
+
+        quick(numbers);
+        System.out.print("快速排序后：");
+        printArr(numbers);
     }
 }
