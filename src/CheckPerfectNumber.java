faed0b9b22bb84303c6ebf6cbd68fc7584bd19e9
Adrian Xu <741996133@qq.com>
2019-03-06 23:37:32 +0800

    commit code

--- src/CheckPerfectNumber.java
+++ src/CheckPerfectNumber.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 对于一个 正整数，如果它和除了它自身以外的所有正因子之和相等，我们称它为“完美数”。
+
+ 给定一个 正整数 n， 如果他是完美数，返回 True，否则返回 False
+ */
 public class CheckPerfectNumber {
     public static boolean checkPerfectNumber(int num) {
         if (num == 0 || num == 1) {
