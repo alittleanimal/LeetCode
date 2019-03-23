a4bf6df837309879919a93c3a1d2d54c594c2bd0
Adrian Xu <741996133@qq.com>
2019-03-23 23:33:05 +0800

    commit code

--- src/numSquares.java
+++ src/numSquares.java
@@ -1,34 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
+/**
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ *
+ * 示例 1:
+ *
+ * 输入: n = 12
+ * 输出: 3
+ * 解释: 12 = 4 + 4 + 4.
+ *
+ * 示例 2:
+ *
+ * 输入: n = 13
+ * 输出: 2
+ * 解释: 13 = 4 + 9.
+ *
+ *
+ * 动态规划
+ * dp[i] = Math.min(dp[i], dp[i-j*j] + 1)
+ * 尝试加上一个完全平方数
+ */
 public class numSquares {
-    public int numSquares(int n) {
-        int sqrt = (int) Math.floor(Math.sqrt(n));
-        List<Integer> squareList = getSquaresList(sqrt);
-
-        int sum = 0;
-        int count = 0;
-        for (Integer square: squareList) {
-            sum+=square;
-            if (sum < n) {
-                count ++;
-                continue;
-            } else if (sum > n) {
-                sum -= square;
-                continue;
-            } if (sum == n) {
-                count ++;
-                break;
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
-        return count;  
+        return dp[n];
     }
 
-    public List<Integer> getSquaresList(int n) {
-        List<Integer> squareList = new ArrayList<>();
-        for (int i=1; i<=n; i++) {
-            squareList.add((int) Math.pow(i, 2));
-        }
-        return squareList;
+    public static void main(String[] args) {
+
+        System.out.println(numSquares(1));
     }
 }
