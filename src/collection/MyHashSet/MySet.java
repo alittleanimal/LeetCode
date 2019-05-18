6382d3565b0600ae89eaac25932a16f62aa8a361
Adrian Xu <741996133@qq.com>
2019-05-19 06:22:01 +0800

    my hash set

---
+++ src/collection/MyHashSet/MySet.java
@@ -0,0 +1,13 @@
+package collection.MyHashSet;
+
+import java.util.Iterator;
+
+public interface MySet<E> {
+
+    boolean add(E e);
+    boolean remove(E e);
+    boolean contains(Object o);
+    int size();
+    boolean isEmpty();
+    Iterator<E> iterator();
+}
