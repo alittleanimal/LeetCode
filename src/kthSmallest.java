098d6b9abdf60fb9435fd594a05f489f809e87d3
zhongwei.xu <741996133@qq.com>
2019-07-02 00:36:55 +0800

    commit code

---
+++ src/kthSmallest.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+
+ 说明：
+ 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+
+ 示例 1:
+
+ 输入: root = [3,1,4,null,2], k = 1
+ 3
+ / \
+ 1   4
+ \
+ 2
+ 输出: 1
+
+ 示例 2:
+
+ 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ 5
+ / \
+ 3   6
+ / \
+ 2   4
+ /
+ 1
+ 输出: 3
+
+ 进阶：
+ 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class kthSmallest {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> resultList = new ArrayList<>();
+        dfs(root, resultList);
+
+        return resultList.get(k-1);
+    }
+
+    private void dfs (TreeNode root, List<Integer> nums) {
+        if (root.left != null)
+            dfs(root.left, nums);
+        nums.add(root.val);
+        if (root.right != null)
+            dfs(root.right, nums);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
