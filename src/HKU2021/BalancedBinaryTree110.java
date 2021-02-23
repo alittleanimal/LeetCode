22f9caf20eae90d35fb71d07ceb409f563629102
Adrian Xu <741996133@qq.com>
2021-02-23 23:59:30 +0800

    commit 2021 HKU leetcode 0223

---
+++ src/HKU2021/BalancedBinaryTree110.java
@@ -0,0 +1,58 @@
+package HKU2021;
+
+public class BalancedBinaryTree110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1
+                    && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return Math.max(height(node.left), height(node.right)) + 1;
+        }
+    }
+
+    public boolean isBalancedUnder(TreeNode root) {
+        return heightUnder(root) >= 0;
+    }
+
+    private int heightUnder(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftHeight = heightUnder(root.left);
+        int rightHeight = heightUnder(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+}
