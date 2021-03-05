0224ec58463ccaa30a237b11d9287c48b41e548a
Adrian Xu <741996133@qq.com>
2021-03-05 18:04:46 +0800

    commit 2021 HKU leetcode 0305

--- src/HKU2021/LongestIncreasingSubsequence300.java
+++ src/HKU2021/LongestIncreasingSubsequence300.java
@@ -19,8 +19,37 @@ public class LongestIncreasingSubsequence300 {
         return max;
     }
 
+    public int lengthOfLISBetter(int[] nums) {
+        int len = 1, n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+
     @Test
     public void test300() {
-        System.out.println(lengthOfLIS(new int[]{7,7,7,7,7,7,7}));
+        System.out.println(lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}));
     }
 }
---
+++ src/HKU2021/March/RussianDollEnvelopes354.java
@@ -0,0 +1,81 @@
+package HKU2021.March;
+
+import org.junit.Test;
+
+import java.util.*;
+
+
+public class RussianDollEnvelopes354 {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            } else {
+                return o2[1] - o1[1];
+            }
+        });
+
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max(ans, f[i]);
+        }
+        return ans;
+    }
+
+    public int maxEnvelopesBetter(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, (o1, o2) -> {
+            if (o1[0] != o2[0]) {
+                return o1[0] - o2[0];
+            } else {
+                return o2[1] - o1[1];
+            }
+        });
+
+        List<Integer> f = new ArrayList<>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; ++i) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)) {
+                f.add(num);
+            } else {
+                int index = binarySearch(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    private int binarySearch(List<Integer> f, int num) {
+        int low = 0, high = f.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < num)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+
+    @Test
+    public void test354() {
+        System.out.println(maxEnvelopesBetter(new int[][]{{4, 5}, {4, 6}, {6, 7}, {2, 3}, {1, 1}}));
+    }
+}
